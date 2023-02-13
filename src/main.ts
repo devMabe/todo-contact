ebaae301480a5f1438c108df08187e772c58d5a0
Mateo Beltran <matymatbeltran@hotmail.com>
2023-02-13 11:21:09 -0500

    clean code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.enableCors()
+  const PORT = parseInt(process.env.PORT) || 3000
+  await app.listen(PORT)
 }
 bootstrap()
