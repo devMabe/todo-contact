0eea9496aedf00dc9cb4b0b32d2ba027c2413175
Mateo Beltran <matymatbeltran@hotmail.com>
2023-02-16 15:41:48 -0500

    Documentation swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import 'dotenv/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const options = new DocumentBuilder()
+    .setTitle('Todo-Contacts')
+    .setDescription('Api restFull todo-Contacts')
+    .setVersion('1.0')
+    .addTag('contacts')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+
+  SwaggerModule.setup('api/docs', app, document)
+
   app.enableCors()
   const PORT = parseInt(process.env.PORT) || 3000
   await app.listen(PORT)
